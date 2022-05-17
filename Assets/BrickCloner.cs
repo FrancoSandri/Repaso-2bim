a734401f3c5a93ca23773c4a6aa3b0b793679565
FrancoSandri <46960579@est.ort.edu.ar>
2022-05-17 15:58:20 -0300

    brickClone

---
+++ Assets/BrickCloner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float xOffset;
+    int counter = 0;
+    public Text userOffset;
+
+
+    public void CloneBrick()
+    {
+        if(counter == 0)
+        {
+            xOffset = float.Parse(userOffset.text);
+        }
+
+        if (counter < xOffset)
+        {
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            clon.transform.position = new Vector3(float.Parse(userOffset.text), 0, 0);
+            xOffset += 1.1f;
+            counter++;
+        }
+    }
+}
